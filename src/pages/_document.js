d9a5c7f52b9b971b46c911cc0150d2a60220dea8
vladimir <vladimirmuratov@mail.ru>
2025-06-25 16:28:59 +0300

    fix index.js head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,29 @@ export default function Document(props) {
             <Head>
                 <DocumentHeadTags {...props} />
                 <meta name="yandex-verification" content="bbac592db0eece12" />
+                <meta
+                    name="description"
+                    content="Госпитализация в ведущие столичные клиники в кратчайшие сроки. Помощь при острых и хронических состояниях. Работаем с частными и государственными учреждениями."
+                />
+                <meta
+                    name="keywords"
+                    content="платная госпитализация, гинекология госпитализация, платная палата в хирургии, травматология платная палата, платная госпитализация урология, платный стационар кардиология, платный стационар неврологии,  платная госпитализация в москве гастроэнтерология, больница терапия платная палата"
+                />
+                <link rel="shortcut icon" href="/favicon.ico" type="image/ico" />
+                <link rel="canonical" href="https://msk-group-hospital.ru" />
+                <meta property="og:locale" content="ru_RU" />
+                <meta property="og:title"
+                      content="Медицинская Сервисная Компания. Срочная платная госпитализация в Москве — помощь без ожидания. 8 (499) 719-81-00, 24/7" />
+                <meta property="og:description"
+                      content="Госпитализация в ведущие столичные клиники в кратчайшие сроки. Помощь при острых и хронических состояниях. Работаем с частными и государственными учреждениями." />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://msk-ambulance.ru/" />
+                <meta property="og:image" content="https://msk-ambulance.ru/images/banner-3.webp" />
+                <meta property="og:image:alt"
+                      content="Медицинская Сервисная Компания. Срочная платная госпитализация в Москве — помощь без ожидания. 8 (499) 719-81-00, 24/7" />
+                <meta property="og:image:type" content="image/webp" />
+                <meta property="og:site_name"
+                      content="Медицинская Сервисная Компания. Срочная платная госпитализация в Москве — помощь без ожидания. 8 (499) 719-81-00, 24/7" />
             </Head>
             <body>
             <Main />
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,30 +9,7 @@ export default function Home() {
             <Head>
                 <title>Медицинская Сервисная Компания. Срочная платная госпитализация в Москве — помощь без ожидания. 8
                     (499) 719-81-00, 24/7</title>
-                <meta
-                    name="description"
-                    content="Госпитализация в ведущие столичные клиники в кратчайшие сроки. Помощь при острых и хронических состояниях. Работаем с частными и государственными учреждениями."
-                />
-                <meta
-                    name="keywords"
-                    content="платная госпитализация, гинекология госпитализация, платная палата в хирургии, травматология платная палата, платная госпитализация урология, платный стационар кардиология, платный стационар неврологии,  платная госпитализация в москве гастроэнтерология, больница терапия платная палата"
-                />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="shortcut icon" href="/favicon.ico" type="image/ico" />
-                <link rel="canonical" href="https://msk-group-hospital.ru" />
-                <meta property="og:locale" content="ru_RU" />
-                <meta property="og:title"
-                      content="Медицинская Сервисная Компания. Срочная платная госпитализация в Москве — помощь без ожидания. 8 (499) 719-81-00, 24/7" />
-                <meta property="og:description"
-                      content="Госпитализация в ведущие столичные клиники в кратчайшие сроки. Помощь при острых и хронических состояниях. Работаем с частными и государственными учреждениями." />
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://msk-ambulance.ru/" />
-                <meta property="og:image" content="https://msk-ambulance.ru/images/banner-3.webp" />
-                <meta property="og:image:alt"
-                      content="Медицинская Сервисная Компания. Срочная платная госпитализация в Москве — помощь без ожидания. 8 (499) 719-81-00, 24/7" />
-                <meta property="og:image:type" content="image/webp" />
-                <meta property="og:site_name"
-                      content="Медицинская Сервисная Компания. Срочная платная госпитализация в Москве — помощь без ожидания. 8 (499) 719-81-00, 24/7" />
             </Head>
             <main>
                 <Suspense>
