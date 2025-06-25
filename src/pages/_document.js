ef5f8b738377b6ad48818124db4c2f89cdeb53ce
vladimir <vladimirmuratov@mail.ru>
2025-06-25 16:12:30 +0300

    fix yandex-verification

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,21 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import { DocumentHeadTags, documentGetInitialProps } from '@mui/material-nextjs/v14-pagesRouter';
+import { Head, Html, Main, NextScript } from 'next/document';
+import { documentGetInitialProps, DocumentHeadTags } from '@mui/material-nextjs/v14-pagesRouter';
 
 export default function Document(props) {
-  return (
-    <Html lang="en" prefix="og: https://ogp.me/ns#">
-      <Head>
-        <DocumentHeadTags {...props} />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en" prefix="og: https://ogp.me/ns#">
+            <Head>
+                <DocumentHeadTags {...props} />
+                <meta name="yandex-verification" content="bbac592db0eece12" />
+            </Head>
+            <body>
+            <Main />
+            <NextScript />
+            </body>
+        </Html>
+    );
 }
 
 Document.getInitialProps = async (ctx) => {
-  const finalProps = await documentGetInitialProps(ctx);
-  return finalProps;
+    const finalProps = await documentGetInitialProps(ctx);
+    return finalProps;
 };
