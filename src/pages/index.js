62ac96c842e9327eadbabdd444b9b7b1d374cda2
vladimir <vladimirmuratov@mail.ru>
2025-07-07 14:05:23 +0300

    update title description 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
     return (
         <>
             <Head>
+                <title>Медицинская Сервисная Компания. Платная госпитализация. Клиники управления делами Президента РФ. 8 (499) 719-81-00, 24/7</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
             <main>
