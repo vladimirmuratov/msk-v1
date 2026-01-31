fff65d21a1141265c6674f464299486e1b052eb7
vladimir <vladimirmuratov@mail.ru>
2026-01-31 11:11:04 +0300

    update canonical

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,6 @@ export default function Document(props) {
         <Html lang="en" prefix="og: https://ogp.me/ns#">
             <Head>
                 <DocumentHeadTags {...props} />
-                <meta name="yandex-verification" content="bbac592db0eece12" />
                 <title>Все лучшие клиники Москвы в одном месте. Платная госпитализация. 8 (499) 719-81-00, 24/7</title>
                 <meta
                     name="description"
@@ -17,22 +16,22 @@ export default function Document(props) {
                     content="платная госпитализация, гинекология госпитализация, платная палата в хирургии, травматология платная палата, платная госпитализация урология, платный стационар кардиология, платный стационар неврологии,  платная госпитализация в москве гастроэнтерология, больница терапия платная палата"
                 />
                 <link rel="shortcut icon" href="/favicon.ico" type="image/ico" />
-                <link rel="canonical" href="https://msk-group-hospital.ru" />
                 <meta property="og:locale" content="ru_RU" />
                 <meta property="og:title"
                       content="Медицинская Сервисная Компания. Все лучшие клиники Москвы в одном месте. Платная госпитализация. 8 (499) 719-81-00, 24/7" />
                 <meta property="og:description"
                       content="Госпитализация в ведущие клиники Москвы в кратчайшие сроки. Помощь при острых и хронических состояниях. Работаем с частными и государственными учреждениями. Медицинский туризм." />
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://msk-ambulance.ru/" />
-                <meta property="og:image" content="https://msk-ambulance.ru/images/banner-3.webp" />
+                <meta property="og:url" content="https://msk-ambulance.vercel.app" />
+                <meta property="og:image" content="https://msk-ambulance.vercel.app/images/banner-3.webp" />
                 <meta property="og:image:alt"
                       content="Медицинская Сервисная Компания. Все лучшие клиники Москвы в одном месте. Платная госпитализация. 8 (499) 719-81-00, 24/7" />
                 <meta property="og:image:type" content="image/webp" />
                 <meta property="og:site_name"
                       content="Медицинская Сервисная Компания. Все лучшие клиники Москвы в одном месте. Платная госпитализация. 8 (499) 719-81-00, 24/7" />
-                <script type="text/javascript">window._ab_id_=160560</script>
-                <script src="https://cdn.botfaqtor.ru/one.js"></script>
+
+                <meta name="yandex-verification" content="cafce7596a3a082e" />
+                <link rel="canonical" href="https://msk-ambulance.vercel.app" />
             </Head>
             <body>
             <Main />
